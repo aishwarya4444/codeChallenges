630bd305377db3a5bc1f724ae428a9e73d977208
Aishwarya <aishwarya4444@gmail.com>
2019-03-03 15:08:00 +0530

    Using 'GetMapping' instead of 'Request Mapping'

--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/HelloWorldController.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.aisingh.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-  @RequestMapping(method = RequestMethod.GET, path = "/helloWorld")
+
+  @GetMapping(path = "/helloWorld")
   public String helloWorld() {
     return "Hello World";
   }
