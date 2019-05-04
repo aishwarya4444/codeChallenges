bfd4cf892b97fa22356f0e5eb040e6c75fb561b9
Aishwarya <aishwarya4444@gmail.com>
2019-05-04 10:27:03 +0530

    i18n : Internationalization
    Need to check one case, if 'Accept-Language' is not given, 400 is sent

--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,12 @@ package com.aisingh.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +16,17 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.aisingh.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+  @Autowired
+  private MessageSource messageSource;
 
   @GetMapping(path = "/helloWorld")
   public String helloWorld() {
@@ -20,5 +28,10 @@ public class HelloWorldController {
   @GetMapping(path = "/helloWorld/path-variable/{user}")
   public HelloWorldBean helloWorldBean(@PathVariable String user) {
     return new HelloWorldBean(String.format("Hello World, %s", user));
+  }
+
+  @GetMapping(path = "/helloWorld_i18")
+  public String helloWorld_i18(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+    return messageSource.getMessage("good.morning.message", null, locale);
   }
 }
