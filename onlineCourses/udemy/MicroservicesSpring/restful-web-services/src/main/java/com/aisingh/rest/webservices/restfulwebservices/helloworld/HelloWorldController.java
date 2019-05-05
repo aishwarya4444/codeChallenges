7675533030c3afa03341b8d3989fbb44abe6eb1a
Aishwarya <aishwarya4444@gmail.com>
2019-05-05 14:39:57 +0530

    i18n set messages in properties file
    alternative without using 'Accept-Language'

--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.aisingh.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -31,7 +32,7 @@ public class HelloWorldController {
   }
 
   @GetMapping(path = "/helloWorld_i18")
-  public String helloWorld_i18(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-    return messageSource.getMessage("good.morning.message", null, locale);
+  public String helloWorld_i18() {
+    return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
   }
 }
