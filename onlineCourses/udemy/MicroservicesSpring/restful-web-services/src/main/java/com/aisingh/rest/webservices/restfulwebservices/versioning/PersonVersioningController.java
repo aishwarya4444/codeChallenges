0b39a4708db79c01d111b10e4d8eb763cee0b2a2
Aishwarya <aishwarya4444@gmail.com>
2019-05-13 22:21:22 +0530

    Versioning using 'query param', 'headers', 'produces (or MIME)'
    [1] http://urthen.github.io/2013/05/09/ways-to-version-your-api/
    [2] https://www.mnot.net/blog/2011/10/25/web_api_versioning_smackdown
    [3] https://stackoverflow.com/questions/389169/best-practices-for-api-versioning

--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -15,4 +15,34 @@ public class PersonVersioningController {
   public PersonV2 personV2() {
     return new PersonV2(new Name("Bob", "Charlie"));
   }
+
+  @GetMapping(value="person", params="version=1")
+  public PersonV1 paramV1() {
+    return new PersonV1("Alpha Charlie");
+  }
+
+  @GetMapping(value="person", params="version=2")
+  public PersonV2 paramV2() {
+    return new PersonV2(new Name("Alpha", "Charlie"));
+  }
+
+  @GetMapping(value="person", headers="X-API-VERSION=1")
+  public PersonV1 headersV1() {
+    return new PersonV1("Gama Charlie");
+  }
+
+  @GetMapping(value="person", headers="X-API-VERSION=2")
+  public PersonV2 headersV2() {
+    return new PersonV2(new Name("Gama", "Charlie"));
+  }
+
+  @GetMapping(value="person", produces="application/vnd.company.app-v1+json")
+  public PersonV1 producesV1() {
+    return new PersonV1("Tango Charlie");
+  }
+
+  @GetMapping(value="person", produces="application/vnd.company.app-v2+json")
+  public PersonV2 producesV2() {
+    return new PersonV2(new Name("Tango", "Charlie"));
+  }
 }
