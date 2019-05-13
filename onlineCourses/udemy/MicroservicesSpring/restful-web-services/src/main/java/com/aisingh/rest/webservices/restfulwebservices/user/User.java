32e7334fdaa7c7e089f58eeca96739292487042b
Aishwarya <aishwarya4444@gmail.com>
2019-05-14 00:07:13 +0530

    Setting up h2 and disabling security
    [1] http://localhost:8080/h2-console
    [2] JDBC URL should be jdbc:h2:mem:testdb

--- onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/user/User.java
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,17 @@
 package com.aisingh.rest.webservices.restfulwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User {
 
+  @Id
+  @GeneratedValue
   private Integer id;
 
   @Size(min = 2, message = "Name should have atleast 2 characters")
---
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user VALUES (1, sysdate(), 'Charmy');
+INSERT INTO user VALUES (2, sysdate(), 'Jolly');
+INSERT INTO user VALUES (3, sysdate(), 'TukTuk');
