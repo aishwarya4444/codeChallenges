f4545d644a01d66bd09fe22bedb9fa9fd083e121
Aishwarya <aishwarya4444@gmail.com>
2019-05-13 21:51:54 +0530

    Versioning with URI

---
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,21 @@
+package com.aisingh.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+
+  private Name name;
+
+  public PersonV2() {
+  }
+
+  public PersonV2(Name name) {
+    this.name = name;
+  }
+
+  public Name getName() {
+    return name;
+  }
+
+  public void setName(Name name) {
+    this.name = name;
+  }
+}
---
+++ onlineCourses/udemy/MicroservicesSpring/restful-web-services/src/main/java/com/aisingh/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,18 @@
+package com.aisingh.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+  @GetMapping("v1/person")
+  public PersonV1 personV1() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping("v2/person")
+  public PersonV2 personV2() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+}
