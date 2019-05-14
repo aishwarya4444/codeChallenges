936be34f787af0bec6d2acf8a36b60f548ad600e
Aishwarya <aishwarya4444@gmail.com>
2019-05-14 19:47:25 +0530

    Create a hard coded limits service
    http://localhost:8080/limits

---
+++ onlineCourses/udemy/MicroservicesSpring/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,31 @@
+package com.in28minutes.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+  private int maximum;
+  private int minimum;
+
+  public LimitConfiguration(int maximum, int minimum) {
+    this.maximum = maximum;
+    this.minimum = minimum;
+  }
+
+  protected LimitConfiguration() {
+  }
+
+  public int getMaximum() {
+    return maximum;
+  }
+
+  public void setMaximum(int maximum) {
+    this.maximum = maximum;
+  }
+
+  public int getMinimum() {
+    return minimum;
+  }
+
+  public void setMinimum(int minimum) {
+    this.minimum = minimum;
+  }
+}
+
