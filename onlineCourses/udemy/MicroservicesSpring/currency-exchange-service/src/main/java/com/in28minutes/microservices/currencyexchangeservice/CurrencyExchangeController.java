76df8a03e7f89ef6a9a0b2772e02c468dac3b692
Aishwarya <aishwarya4444@gmail.com>
2019-05-15 20:17:26 +0530

    Read 'from' and 'to' from URL and get result from JPA

--- onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -14,9 +14,12 @@ public class CurrencyExchangeController {
   @Autowired
   private Environment environment;
 
+  @Autowired
+  private ExchangeValueRepository repository;
+
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-    ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+    ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
     exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
     return exchangeValue;
   }
