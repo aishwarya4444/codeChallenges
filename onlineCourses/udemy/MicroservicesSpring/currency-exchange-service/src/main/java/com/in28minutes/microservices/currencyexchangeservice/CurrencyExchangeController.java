4e4ece9e440595f6d2c981ed9b65697309864ff7
Aishwarya <aishwarya4444@gmail.com>
2019-05-15 19:34:32 +0530

    Return port no. in response
    Specify 'Dserver.port=8001' under 'VM Options' if you want port different from the one mentioned in application.properties

--- onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,17 +1,23 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.in28minutes.microservices.currencyexchangeservice.ExchangeValue;
 
 import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
 
+  @Autowired
+  private Environment environment;
+
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-    return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+    ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+    exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    return exchangeValue;
   }
 }
--- onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -8,6 +8,7 @@ public class ExchangeValue {
   private String from;
   private String to;
   private BigDecimal conversionMultiple;
+  private int port;
 
   public ExchangeValue() {
   }
@@ -49,5 +50,13 @@ public class ExchangeValue {
 
   public void setConversionMultiple(BigDecimal conversionMultiple) {
     this.conversionMultiple = conversionMultiple;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
   }
 }
