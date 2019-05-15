cca414082180ea81ff52cb0370b61611025d6297
Aishwarya <aishwarya4444@gmail.com>
2019-05-15 19:54:53 +0530

    Setting up h2 for currency-exchange-service

--- onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ onlineCourses/udemy/MicroservicesSpring/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,12 +1,22 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
 
+  @Id
   private Long id;
+
+  @Column(name="currency_from")
   private String from;
+
+  @Column(name="currency_to")
   private String to;
+
   private BigDecimal conversionMultiple;
   private int port;
 
