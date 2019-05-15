7c05bec5f20dcc165e39f336ad550426a5813a69
Aishwarya <aishwarya4444@gmail.com>
2019-05-15 20:41:14 +0530

    Setup currency-conversion-service

---
+++ onlineCourses/udemy/MicroservicesSpring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,83 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+
+  private Long Id;
+  private String from;
+  private String to;
+  private BigDecimal conversionMultiple;
+  private BigDecimal quantity;
+  private BigDecimal totalCalculatedAmount;
+  private int port;
+
+  public CurrencyConversionBean() {
+  }
+
+  public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal totalCalculatedAmount, int port) {
+    Id = id;
+    this.from = from;
+    this.to = to;
+    this.conversionMultiple = conversionMultiple;
+    this.quantity = quantity;
+    this.totalCalculatedAmount = totalCalculatedAmount;
+    this.port = port;
+  }
+
+  public Long getId() {
+    return Id;
+  }
+
+  public void setId(Long id) {
+    Id = id;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public BigDecimal getConversionMultiple() {
+    return conversionMultiple;
+  }
+
+  public void setConversionMultiple(BigDecimal conversionMultiple) {
+    this.conversionMultiple = conversionMultiple;
+  }
+
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(BigDecimal quantity) {
+    this.quantity = quantity;
+  }
+
+  public BigDecimal getTotalCalculatedAmount() {
+    return totalCalculatedAmount;
+  }
+
+  public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+    this.totalCalculatedAmount = totalCalculatedAmount;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+}
