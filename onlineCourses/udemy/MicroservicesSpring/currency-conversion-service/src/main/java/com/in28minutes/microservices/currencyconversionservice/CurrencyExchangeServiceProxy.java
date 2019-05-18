f63f02865136aceda151179df40b20d498fdd483
Aishwarya <aishwarya4444@gmail.com>
2019-05-19 03:22:51 +0530

    Setting up Zuul API gateway between microservice invocation
    Now instead of calling currency-conversion-service at http://localhost:8100/currency-converter-feign/from/USD/to/INR/quantity/10 I can directly call Zuul API at http://localhost:8765/currency-conversion-service/currency-converter-feign/from/USD/to/INR/quantity/10

--- onlineCourses/udemy/MicroservicesSpring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ onlineCourses/udemy/MicroservicesSpring/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // as ribbon is used, we don't need URL in feign
-@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
