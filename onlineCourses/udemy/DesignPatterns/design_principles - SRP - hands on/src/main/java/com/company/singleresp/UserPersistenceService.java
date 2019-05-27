8c44f0f108aedee7c2f0855102d67d76ed1455c9
Aishwarya <aishwarya4444@gmail.com>
2019-05-28 03:12:07 +0530

    SRP end
    Single Responsibility Principle

--- onlineCourses/udemy/DesignPatterns/design_principles - SRP - hands on/src/main/java/com/company/singleresp/UserPersistenceService.java
+++ onlineCourses/udemy/DesignPatterns/design_principles - SRP - hands on/src/main/java/com/company/singleresp/UserPersistenceService.java
@@ -2,5 +2,11 @@ package com.company.singleresp;
 
 public class UserPersistenceService {
 
+  //Store used by controller
+  private Store store = new Store();
+
+  public void saveUser(User user) {
+    store.store(user);
+  }
 	
 }
