b6bb8f55e386ac2724260bd89de90d9fc6893b0b
Aishwarya <aishwarya4444@gmail.com>
2019-05-28 19:32:21 +0530

    dependency inv - end
    'Autowiring' in Spring uses dependency inv

--- onlineCourses/udemy/DesignPatterns/design_principles - dependency inv/src/main/java/com/company/depinv/Main.java
+++ onlineCourses/udemy/DesignPatterns/design_principles - dependency inv/src/main/java/com/company/depinv/Main.java
@@ -1,14 +1,17 @@
 package com.company.depinv;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Message msg = new Message("This is a message again");
+		Message msg = new Message("This is a new message");
 		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.txt");
+		printer.writeMessage(msg, new JSONFormatter(), new PrintWriter(new FileWriter("test_msg.txt")));
+		printer.writeMessage(msg, new JSONFormatter(), new PrintWriter(System.out));
 	}
 
 }
--- onlineCourses/udemy/DesignPatterns/design_principles - dependency inv/src/main/java/com/company/depinv/MessagePrinter.java
+++ onlineCourses/udemy/DesignPatterns/design_principles - dependency inv/src/main/java/com/company/depinv/MessagePrinter.java
@@ -7,12 +7,8 @@ import java.io.PrintWriter;
 public class MessagePrinter {
 
 	//Writes message to a file
-	public void writeMessage(Message msg, String fileName) throws IOException {
-		Formatter formatter = new JSONFormatter();//creates formatter
-		try(PrintWriter writer = new PrintWriter(new FileWriter(fileName))) { //creates print writer
-			writer.println(formatter.format(msg)); //formats and writes message
-			writer.flush();
-			writer.close();
-		}
+	public void writeMessage(Message msg, Formatter formatter, PrintWriter writer) throws IOException {
+		writer.println(formatter.format(msg));
+		writer.flush();
 	}
 }
