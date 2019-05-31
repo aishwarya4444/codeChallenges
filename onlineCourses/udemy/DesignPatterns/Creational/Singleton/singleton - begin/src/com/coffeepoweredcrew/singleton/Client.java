d9169bd5f7efa390b72162e3766cdf61f1c7cf79
Aishwarya <aishwarya4444@gmail.com>
2019-05-31 23:59:45 +0530

    Singleton - eager registry

--- onlineCourses/udemy/DesignPatterns/Creational/Singleton/singleton - begin/src/com/coffeepoweredcrew/singleton/Client.java
+++ onlineCourses/udemy/DesignPatterns/Creational/Singleton/singleton - begin/src/com/coffeepoweredcrew/singleton/Client.java
@@ -4,6 +4,9 @@ public class Client {
 
 	public static void main(String[] args) {
 
+		EagerRegistry registry = EagerRegistry.getInstance();
+		EagerRegistry registry2 = EagerRegistry.getInstance();
+		System.out.println(registry == registry2);
 	}
 
 }
--- onlineCourses/udemy/DesignPatterns/Creational/Singleton/singleton - begin/src/com/coffeepoweredcrew/singleton/EagerRegistry.java
+++ onlineCourses/udemy/DesignPatterns/Creational/Singleton/singleton - begin/src/com/coffeepoweredcrew/singleton/EagerRegistry.java
@@ -3,6 +3,15 @@ package com.coffeepoweredcrew.singleton;
  * This class uses eager initialization of singleton instance.
  */
 public class EagerRegistry {
-	
+
+  private EagerRegistry() {
+    // no-op
+  }
+
+  private static final EagerRegistry INSTANCE = new EagerRegistry();
+
+  public static EagerRegistry getInstance() {
+    return INSTANCE;
+  }
 
 }
