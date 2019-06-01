55f92811632cd7bf7760fb56f8ef157cf4dcb3f1
Aishwarya <aishwarya4444@gmail.com>
2019-06-01 14:30:45 +0530

    Simple Factory - end

--- onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/Client.java
+++ onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/Client.java
@@ -3,8 +3,17 @@ package com.coffeepoweredcrew.simplefactory;
 public class Client {
 
 	public static void main(String[] args) {
+		Post post = PostFactory.createPost("blog");
+		System.out.println(post);
 		
-
+		post = PostFactory.createPost("news");
+		System.out.println(post);
+		
+		post = PostFactory.createPost("product");
+		System.out.println(post);
+		
+		post = PostFactory.createPost("my post");
+		System.out.println(post);
 	}
 
 }
--- onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/PostFactory.java
+++ onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/PostFactory.java
@@ -6,5 +6,18 @@ package com.coffeepoweredcrew.simplefactory;
  *
  */
 public class PostFactory {
+	
+	public static Post createPost(String type) {
+		switch(type) {
+		case "blog":
+			return new BlogPost();
+		case "news":
+			return new NewsPost();
+		case "product":
+			return new ProductPost();
+		default:
+			throw new IllegalArgumentException("Error : Unknown type provided!");
+		}
+	}
 
 }
