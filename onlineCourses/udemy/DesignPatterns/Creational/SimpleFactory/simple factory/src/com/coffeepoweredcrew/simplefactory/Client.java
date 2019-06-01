f95858f5613bbba7c2d2c15d8c224f5fe9f4e051
Aishwarya <aishwarya4444@gmail.com>
2019-06-01 14:28:07 +0530

    Simple Factory - begin

---
+++ onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/Client.java
@@ -0,0 +1,10 @@
+package com.coffeepoweredcrew.simplefactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ onlineCourses/udemy/DesignPatterns/Creational/SimpleFactory/simple factory/src/com/coffeepoweredcrew/simplefactory/PostFactory.java
@@ -0,0 +1,10 @@
+package com.coffeepoweredcrew.simplefactory;
+
+/**
+ * This class acts as a simple factory for creation of 
+ * different posts on web site.
+ *
+ */
+public class PostFactory {
+
+}
