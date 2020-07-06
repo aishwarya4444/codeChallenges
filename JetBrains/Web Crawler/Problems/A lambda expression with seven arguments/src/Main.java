91b6e64bb73342ab3a599580ae191a80a2877a86
Aishwarya <aishwarya4444@gmail.com>
2020-07-06 22:09:36 +0530

    Web Crawler init

---
+++ JetBrains/Web Crawler/Problems/A lambda expression with seven arguments/src/Main.java
@@ -0,0 +1,8 @@
+class Seven {
+
+    public static MultipleArgumentsLambda.SeptenaryStringFunction fun = //write your code here
+            (a, b, c, d, e, f, g) -> {
+                String res = a+b+c+d+e+f+g;
+                return res.toUpperCase();
+            };
+}
