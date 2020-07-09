4d3c8863349714f6c99f39aac24b51efcb536f0e
Aishwarya <aishwarya4444@gmail.com>
2020-07-09 10:23:48 +0530

    more

---
+++ JetBrains/Web Crawler/Web Crawler/task/src/crawler/WebCrawler.java
@@ -0,0 +1,11 @@
+package crawler;
+
+import javax.swing.*;
+
+public class WebCrawler extends JFrame {
+    public WebCrawler() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+}
