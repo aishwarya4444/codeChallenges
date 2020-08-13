b3ee5f293b2f799098d08b3be751561b40399d13
Aishwarya <aishwarya4444@gmail.com>
2020-08-14 02:06:57 +0530

    correct dp for house robber

--- onlineCourses/educative/Dynamic_Programming/Pattern_3_Fibonacci_Numbers/06_House_Thief/House_DP.cc
+++ onlineCourses/educative/Dynamic_Programming/Pattern_3_Fibonacci_Numbers/06_House_Thief/House_DP.cc
@@ -16,7 +16,7 @@ int solve(vector<int> &house) {
   dp[0] = 0;
   dp[1] = house[0];
   for(i=2; i<=N; i++) {
-    dp[i] = max(dp[i-1], house[i-2]+dp[i-1]);
+    dp[i] = max(dp[i-1], house[i-1]+dp[i-2]);
   }
   return dp[N];
 }
