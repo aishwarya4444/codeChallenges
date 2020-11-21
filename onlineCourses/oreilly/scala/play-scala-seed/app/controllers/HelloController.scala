d26ed488bd510d2ca7e481892301fa48ad56a05c
Aishwarya <aishwarya4444@gmail.com>
2020-11-21 11:36:11 +0530

    POST example
    curl --header 'Content-type: application/x-www-form-urlencoded'  --request GET http://localhost:9000
    curl --header 'Content-type: application/x-www-form-urlencoded'  --request POST --data 'name=Lallan&age=31' http://localhost:9000/addUser

--- onlineCourses/oreilly/scala/play-scala-seed/app/controllers/HelloController.scala
+++ onlineCourses/oreilly/scala/play-scala-seed/app/controllers/HelloController.scala
@@ -11,4 +11,26 @@ class HelloController @Inject()(val controllerComponents: ControllerComponents) 
   def wishByName(name: String) = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.hello(name))
   }
+
+  def addUser() = Action { implicit request: Request[AnyContent] =>
+    val body = request.body;
+
+    body.asFormUrlEncoded match{
+      case Some(map) =>
+        //persist user information
+        Ok(s"The user of name `${map("name").head}` and age `${map("age").head}` has been created\n")
+      case None => BadRequest("Unknow body format")
+    }
+
+    //    Content-type                                     Method Name
+    //
+    //    application/x-www-form-urlencoded                request.body.asFormUrlEncoded
+    //
+    //    text/plain                                       request.body.asText
+    //
+    //    application/xml                                  request.body.asXML
+    //
+    //    application/json                                 request.body.asJson
+
+  }
 }
