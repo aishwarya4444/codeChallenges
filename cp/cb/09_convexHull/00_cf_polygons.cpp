b153f47445753dc96208ec4848d82e07c36e2da8
Aishwarya <aishwarya4444@gmail.com>
2021-08-08 05:32:00 +0530

    convex hull

--- cp/cb/09_convexHull/00_cf_polygons.cpp
+++ cp/cb/09_convexHull/00_cf_polygons.cpp
@@ -10,7 +10,6 @@ https://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/
 
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 
 /*
@@ -59,63 +58,48 @@ upper hull => clock wise case
 slope of (pn, pi) < (pi, p1) .... let (p1, pi, pn) = (a, b, c)
 (c.y - b.y)/(c.x - b.x) < (a.y - b.y)/(a.x - b.x)
 (c.y - b.y)*(a.x - b.x) < (c.x - b.x)*(a.y - b.y)
+
+cy.ax - cy.bx - by.ax + by.bx < cx.ay - cx.by - bx.ay + bx.by
+cy.ax - cy.bx - by.ax < cx.ay - cx.by - bx.ay
+ax(cy - by) - cy.bx < cx(ay - by) - bx.ay
+ax(cy - by) + bx(ay - cy) + cx(by - ay) < 0
+ax(by - cy) + bx(cy - ay) + cx(ay - by) > 0
 */
 
 struct point {
-    int x;
-    int y;
-    bool operator < (point &O) {
-        if (O.x == x) return y < O.y;
-        else return x < O.x;
-    }
-    bool operator == (point &O) {
-        return (x == O.x && y == O.y);
-    }
-
+    ll x;
+    ll y;
+    // ll is used because overflow can happen during + - *
 };
 
-// struct cmpAsc {
-//     bool operator()(const point &p1, const point &p2) {
-//         if(p1.x == p2.x) {
-//             return p1.y<p2.y;
-//         }
-//         return p1.x<p2.x;
-//     }
-// };
+// used for sort() and set<>
+bool operator < (const point &p1, const point &p2) {
+    if(p1.x == p2.x) {
+        return p1.y<p2.y;
+    }
+    return p1.x<p2.x;
+}
 
-vector<point> a;
-vector<point> b;
-// set<point, cmpAsc()> b;
-// set<point, cmpAsc()> hull;
-map<pair<int, int>, int > hull;
+set<point> hull;
+vector<point> a, b;
 int n, m;
 
-// struct cmpEqual {
-//     bool operator()(const point &p1, const point &p2) {
-//         return p1.x==p2.x && p1.y==p2.y;
-//     }
-// };
-
 bool isClockWise(point a, point b, point c) {
-    return (c.y - b.y)*(a.x - b.x) < (c.x - b.x)*(a.y - b.y);
-}
-
-bool isAntiClockWise(point a, point b, point c) {
-    return (c.y - b.y)*(a.x - b.x) > (c.x - b.x)*(a.y - b.y);
-}
-
-bool isCollinear(point a, point b, point c) {
-    return (c.y - b.y)*(a.x - b.x) == (c.x - b.x)*(a.y - b.y);
-}
-bool cw(point a, point b, point c) {
     return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) > 0;
 }
 
-bool ccw(point a, point b, point c) {
+bool isAntiClockWise(point a, point b, point c) {
     return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) < 0;
 }
+
+bool isCollinear(point a, point b, point c) {
+    return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) == 0;
+}
+
 void convexHull() {
     vector<point> lo, up;
+
+    n = a.size();
     sort(a.begin(), a.end());
     point p1=a[0], pn=a.back(), pi;
 
@@ -124,50 +108,40 @@ void convexHull() {
 
     for(int i=1; i<n; i++) {
         pi = a[i];
+
+        // upper half of convex hull
         // if(i==n-1 || isClockWise(p1, pi, pn)) {
-        //     if(up.size()>1 && !isClockWise(up[up.size()-2], up.back(), pn)) {
-        //         up.pop_back();
-        //     }
-        //     up.push_back(pi);
-        // }
-
-        // if(i==n-1 || isAntiClockWise(p1, pi, pn)) {
-        //     if(lo.size()>1 && !isAntiClockWise(lo[lo.size()-1], lo.back(), pn)) {
-        //         lo.pop_back();
-        //     }
-        //     lo.push_back(pi);
-        // }
-
-        if (i == n - 1 || !ccw(p1, pi, pn)) {
-            // p[i] is in the upper half
-            while (up.size() >= 2 && ccw(up[up.size() - 2], up[up.size() - 1], pi)) {
+        // we use !isAntiClockWise() to check for collinear condition also
+        if(i==n-1 || !isAntiClockWise(p1, pi, pn)) {
+            while(up.size()>1 && isAntiClockWise(up[up.size()-2], up.back(), pi)) {
                 up.pop_back();
             }
             up.push_back(pi);
         }
-        if (i == n - 1 || !cw(p1, pi, pn)) {
-            // p[i] is in the lower half
-            while (lo.size() >= 2 && cw(lo[lo.size() - 2], lo[lo.size() - 1], pi)) {
+
+        // lower half of convex hull
+        // if(i==n-1 || isAntiClockWise(p1, pi, pn)) {
+        // we use !isClockWise() to check for collinear condition also
+        if(i==n-1 || !isClockWise(p1, pi, pn)) {
+            while(lo.size()>1 && isClockWise(lo[lo.size()-2], lo.back(), pi)) {
                 lo.pop_back();
             }
             lo.push_back(pi);
         }
     }
 
-    for(auto p: a) {
-        // hull.insert(p);
-        hull[{p.x,p.y}]++;
+    for(auto p: lo) {
+        hull.insert(p);
     }
 
-    for(auto p: b) {
-        // hull.insert(p);
-        hull[{p.x,p.y}]++;
+    for(auto p: up) {
+        hull.insert(p);
     }
 }
 
 int main()
 {
-    int x, y;
+    ll x, y;
 
     cin>>n;
     for(int i=0; i<n; i++) {
@@ -185,17 +159,14 @@ int main()
     convexHull();
 
     for(auto p: b) {
-        if(hull.count({p.x, p.y})) {
+        if(hull.count(p)) {
             cout<<"NO";
             return 0;
         }
-    }
-
-    for(auto p: hull) {
-        cout<<p.first.first<<' '<<p.first.second<<endl;
     }
 
     cout<<"YES";
 
     return 0;
 }
+
